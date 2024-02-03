e2deb386e0014ff1b9cb8b42ce0b257ac4a4904a
Bennett Benedict <bennybenester@gmail.com>
2024-02-04 02:07:19 +0300

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ApolloProvider from "./ApolloProvider";
-import App from "./App";
+import App from "./appa";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
